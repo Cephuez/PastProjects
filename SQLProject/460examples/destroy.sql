6fdbfc17cc6154f91127cf209d4c22ae97013578
Cephuez <46613677+Cephuez@users.noreply.github.com>
2021-08-20 16:42:12 -0700

    Deleted Some Unnecessary Files

--- SQLProject/460examples/destroy.sql
+++
@@ -1,5 +0,0 @@
-drop table CatUser cascade constraints;
-drop table INDIVIDUAL cascade constraints;
-drop table MERCHANDISE cascade constraints;
-drop table ADVERTISES cascade constraints;
-drop table BOOK cascade constraints;
