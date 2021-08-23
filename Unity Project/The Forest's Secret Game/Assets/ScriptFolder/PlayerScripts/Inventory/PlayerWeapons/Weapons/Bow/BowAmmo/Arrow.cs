5ddaac941bdb6bded8633ba26085deb75d87f6ae
Cephuez <46613677+Cephuez@users.noreply.github.com>
2021-08-23 16:42:14 -0700

    Commit Removed Files

--- Unity Project/The Forest's Secret Game/Assets/ScriptFolder/PlayerScripts/Inventory/PlayerWeapons/Weapons/Bow/BowAmmo/Arrow.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Assets.ScriptFolder.PlayerScripts.Inventory.PlayerWeapons.Weapons.Bow.BowAmmo
-{
-    public class Arrow : SubWeapon
-    {
-        // Start is called before the first frame update
-        
-    }
-}
