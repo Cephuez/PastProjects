dd677c3e55f6b98022aa418f9092adc507060673
Cephuez <saul2004966@gmail.com>
2025-01-29 21:58:53 -0700

    Create Class Command
    This will hold all the functions that will take input commands from the user

--- PL SQL Auto Parts Warehouse Project/Python Pandas/Input_Command.py
+++ PL SQL Auto Parts Warehouse Project/Python Pandas/Input_Command.py
@@ -3,14 +3,11 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 class command:
-    cs = "(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1522)) (CONNECT_DATA = (SERVER = DEDICATED) (SERVICE_NAME = orclpdb)))"
-
     #connection=oracledb.connect( user="project", password= "password123", dsn= cs)
     # create engine
-    engine = create_engine('oracle+oracledb://', creator=lambda: connection)
-
-    user_name = ""
-    password = ""
+    engine = ""
+    cs = ""
+    connection = ""
 
     def __init__(self, name):
         self.name = name
@@ -18,7 +15,24 @@ class command:
     def print(self):
         print("Yo What's up")
 
-    def log_in(self)
-        user_name = input("User Name: ")
-        password = input("Password: ")
-        connection=oracledb.connect( user="project", password= "", dsn= cs)
+    '''
+    For the sake of debugging, I will have the username and password already put in correctly
+    '''
+    def log_in(self):
+        #self.user_name = input("User Name: ")
+        #self.password = input("Password: ")
+        user_name = "Project"
+        password = "password123"
+        cs = "(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1522)) (CONNECT_DATA = (SERVER = DEDICATED) (SERVICE_NAME = orclpdb)))"
+        self.connection = oracledb.connect( user=user_name, password= password, dsn= cs)
+        self.engine = create_engine('oracle+oracledb://', creator=lambda: self.connection)
+
+    def read_command(self):
+        #query = input("Do Something")
+        query = "SELECT EMPLOYEE_ID, FIRST_NAME, LAST_NAME FROM EMPLOYEES ORDER BY 1"
+        df = pd.read_sql(query, self.engine)
+        # test data prep
+        df_test = df.copy()
+        # export test DataFrame with pandas to_sql
+        df_test.to_sql('test', con = self.engine, if_exists='replace', index=False)
+        print(df)
--- PL SQL Auto Parts Warehouse Project/Python Pandas/Oracle_Connetion.py
+++ PL SQL Auto Parts Warehouse Project/Python Pandas/Oracle_Connetion.py
@@ -4,27 +4,42 @@ from Input_Command import command
 
 from sqlalchemy import create_engine
 
-#    dsn= "DESKTOP-J91G8VC/orclpdb"
 
-# connect to oracle database
-# dsn= "project@ORCLPDB"
-cs = "(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1522)) (CONNECT_DATA = (SERVER = DEDICATED) (SERVICE_NAME = orclpdb)))"
-
-connection=oracledb.connect( user="project", password= "password123", dsn= cs)
-# create engine
-engine = create_engine('oracle+oracledb://', creator=lambda: connection)
-# Get the data into a DataFrame
-query = "";
+query = "1";
 input_command = command("Class")
 input_command.print()
+input_command.log_in()
+input_command.read_command()
 
 
+'''
+# connect to oracle database
+cs = "(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1522)) (CONNECT_DATA = (SERVER = DEDICATED) (SERVICE_NAME = orclpdb)))"
+connection=oracledb.connect( user="project", password= "password123", dsn= cs)
+
+
+#create engine
+print(connection)
+engine = create_engine('oracle+oracledb://', creator=lambda: connection)
+#Get the data into a DataFrame
+input_command.read_command()
+query = input("Type Command")
+query = "SELECT EMPLOYEE_ID, FIRST_NAME, LAST_NAME FROM EMPLOYEES ORDER BY 1"
+df = pd.read_sql(query, engine)
+# test data prep
+df_test = df.copy()
+ export test DataFrame with pandas to_sql
+df_test.to_sql('test', con = engine, if_exists='replace', index=False)
+print(df)
+'''
+'''
 while query != "1":
     query = input("Type Command")
-    #query = "SELECT EMPLOYEE_ID, FIRST_NAME, LAST_NAME FROM EMPLOYEES ORDER BY 1"
+    query = "SELECT EMPLOYEE_ID, FIRST_NAME, LAST_NAME FROM EMPLOYEES ORDER BY 1"
     df = pd.read_sql(query, engine)
     # test data prep
     df_test = df.copy()
     # export test DataFrame with pandas to_sql
     df_test.to_sql('test', con = engine, if_exists='replace', index=False)
     print(df)
+'''
