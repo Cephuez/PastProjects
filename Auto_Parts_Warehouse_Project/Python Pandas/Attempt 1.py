b282f960ad256869520eafacb90e7490b8be7052
Cephuez <saul2004966@gmail.com>
2025-02-06 14:43:36 -0700

    Delete Attempt 1.py

--- Auto_Parts_Warehouse_Project/Python Pandas/Attempt 1.py
+++
@@ -1,9 +0,0 @@
-import oracledb
-import getpass
-
-userpassword = 'fafiluas121'
-
-connection = oracledb.connect(user="Project@orclpdb", password=userpassword,
-                              host="DESKTOP-J91G8VC", port=1521, service_name="orcl")
-
-print(userpassword)
