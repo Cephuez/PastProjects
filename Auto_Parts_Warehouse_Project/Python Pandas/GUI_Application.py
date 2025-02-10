88f8c3aa6e1c4e74ba774cd0c914d897f3299e10
Cephuez <saul2004966@gmail.com>
2025-02-10 16:20:26 -0700

    Fixing UI
    I'm trying to make the UI a little better, please adding some shortcuts.

--- Auto_Parts_Warehouse_Project/Python Pandas/GUI_Application.py
+++ Auto_Parts_Warehouse_Project/Python Pandas/GUI_Application.py
@@ -18,13 +18,17 @@ class gui_application:
 
 
     def __init__(self, name):
-        self.name = name
         self.root = tkinter.Tk()
+        #self.name = name
+        #self.root = tkinter.Tk()
         customtkinter.set_appearance_mode("Dark")
         customtkinter.set_default_color_theme("dark-blue")
 
         self.root = customtkinter.CTk()
         self.root.geometry("500x450")
+
+        #self.root = customtkinter.CTk()
+        #self.root.geometry("500x450")
 
     def print(self):
         print("GUI Application Started")
@@ -65,96 +69,93 @@ class gui_application:
         This will display the views by calling separate classes
     
     '''    
-
+    
     ## From here, you will launch seperate classes to handle each window
     def check_input(self, event):
-        input_num = self.f2_entry1.get()
+        input_num = self.f2_command_entry.get()
         #print("Entry: " + input_num)
         size = len(input_num)
-        self.f2_entry1.delete(0,size)
+        self.f2_command_entry.delete(0,size)
         if(input_num == '1'):
-            Picking_Window = Picking_Parts_Window("Name", self.connection, self.engine, self.root, self.frame2)
+            Picking_Window = Picking_Parts_Window("Name", self.connection, self.engine, self.root, self.command_list_frame)
             Picking_Window.load_picking_view(None)
             #self.load_picking_view(None)
         elif(input_num == '2'):
-            Staging_Window = Staging_Parts_Window("Name", self.connection, self.engine, self.root, self.frame2)
+            Staging_Window = Staging_Parts_Window("Name", self.connection, self.engine, self.root, self.command_list_frame)
             Staging_Window.load_staging_view(None)
             #self.load_staging_view(None)
         elif(input_num == '3'):
-            Gather_Window = Gather_Parts_Window("Name", self.connection, self.engine, self.root, self.frame2)
+            Gather_Window = Gather_Parts_Window("Name", self.connection, self.engine, self.root, self.command_list_frame)
             Gather_Window.load_gather_parts_view(None)
 
     def display_command_window(self):
-        #print("1. Start Picking")
-        #print("2. Stage Items")
-        #print("3. Gather Order Parts")
-        #print("4. Move Parts")
-        #print("5. Process Receiving Items")
-        #print("6. Process Order")
-        #print("7. Look Product's Location")
-        #print("0. Exit Program")
-        self.frame1.destroy()
-        self.frame2 = customtkinter.CTkFrame(master = self.root)
-        self.frame2.pack(pady=20, padx=10, fill="y", expand=True)
-
-        self.f2_label_1 = customtkinter.CTkLabel(master=self.frame2, text="Command List", font=("Roboto", 34))
-        self.f2_label_1.pack(pady=30, padx=10)
-
-        self.f2_entry1 = customtkinter.CTkEntry(master=self.frame2)
-        self.f2_entry1.pack(pady=12, padx=10)
-        self.f2_entry1.bind('<Return>', self.check_input)
-
-        self.f2_label_2 = customtkinter.CTkLabel(master=self.frame2, text="1. Start Picking", font=("Roboto", 20), justify="left")
-        self.f2_label_2.pack(pady=1, padx=10)
-
-        self.f2_label_3 = customtkinter.CTkLabel(master=self.frame2, text="2. Stage Items", font=("Roboto", 20))
-        self.f2_label_3.pack(pady=1, padx=10)
-
-        self.f2_label_4 = customtkinter.CTkLabel(master=self.frame2, text="3. Gather Order Parts", font=("Roboto", 20))
-        self.f2_label_4.pack(pady=1, padx=10)
-
-        '''
-        self.f2_label_5 = customtkinter.CTkLabel(master=self.frame2, text="4. Move Parts", font=("Roboto", 20))
-        self.f2_label_5.pack(pady=1, padx=10)
-
-        self.f2_label_6 = customtkinter.CTkLabel(master=self.frame2, text="5. Process Receiving Items", font=("Roboto", 20))
-        self.f2_label_6.pack(pady=1, padx=10)
-
-        self.f2_label_7 = customtkinter.CTkLabel(master=self.frame2, text="6. Process Order", font=("Roboto", 20))
-        self.f2_label_7.pack(pady=1, padx=10)
-
-        self.f2_label_8 = customtkinter.CTkLabel(master=self.frame2, text="7. Search Product's Location", font=("Roboto", 20))
-        self.f2_label_8.pack(pady=1, padx=1)
-
-        self.f2_label_9 = customtkinter.CTkLabel(master=self.frame2, text="8. Log Out", font=("Roboto", 20))
-        self.f2_label_9.pack(pady=1, padx=1)
-        '''
+        self.log_in_frame.pack_forget()
         
-        
-        raise_frame(self.frame2)
+        self.command_list_frame = customtkinter.CTkFrame(self.root)
+        self.command_list_frame.pack(pady=20, padx=10, fill="y", expand=True)
+        self.frame_list.append(self.command_list_frame)
+        self.f_i = self.f_i + 1
+
+        self.f2_label_1 = customtkinter.CTkLabel(self.command_list_frame , text="Command List", font=("Roboto", 40))
+        self.f2_label_1.pack(side='top', pady=10, padx = 1)
+
+        self.command_input_frame = customtkinter.CTkFrame(self.command_list_frame)
+        self.command_input_frame.pack(side='top', anchor = 'w', pady=10, padx=25)
+
+        self.f2_command_entry = customtkinter.CTkEntry(self.command_input_frame, width = 30, font=("Roboto", 20))
+        self.f2_command_entry.pack(side='left', anchor = 'w', pady=1, padx=1)
+        self.f2_command_entry.bind('<Return>', self.check_input)
+
+        customtkinter.CTkLabel(self.command_input_frame, text="- Command", font=("Roboto", 20)).pack(side='left', anchor = 'w', pady=1, padx=10)
+
+        customtkinter.CTkLabel(self.command_list_frame, text="1. Start Picking", font=("Roboto", 20)).pack(side='top', anchor = 'w', pady=1, padx=30)
+
+        customtkinter.CTkLabel(self.command_list_frame, text="2. Stage Items", font=("Roboto", 20)).pack(side='top', anchor = 'w', pady=1, padx=30)
+
+        customtkinter.CTkLabel(master=self.command_list_frame, text="3. Gather Order Parts", font=("Roboto", 20)).pack(side='top', anchor = 'w', pady=1, padx=30)
+
+        self.short_cut_frame = customtkinter.CTkFrame(master = self.command_list_frame)
+        self.short_cut_frame.pack(side='bottom', pady=20, padx=20)
+
+        customtkinter.CTkLabel(master=self.short_cut_frame, text="F3. Exit", font=("Roboto", 20)).pack(side='left', anchor = 'w', pady=1, padx=30)
+
+        self.bin = self.root.bind('<F3>', self.exit_window)
+        raise_frame(self.command_list_frame)     
+
+    def exit_window(self, event):
+        self.root.unbind('<F3>', self.bin)
+        self.frame_list[self.f_i].destroy()
+        self.beginning_screen()
+        self.connection.close()
         
     def beginning_screen(self):
+        #self.display_command_window()
+
         command = ""
-        self.frame1 = customtkinter.CTkFrame(master = self.root)
-        self.frame1.pack(pady=20, padx=60, fill="both", expand=True)
 
-        self.f1_label = customtkinter.CTkLabel(master=self.frame1, text="Login  System", font=("Roboto", 24))
-        self.f1_label.pack(pady=12, padx=10)
+        self.frame_list = []
+        self.f_i = 0;
+        
+        self.log_in_frame = customtkinter.CTkFrame(self.root)
+        self.log_in_frame.pack(pady=1, padx=1, fill="both", expand=True)
+        self.frame_list.append(self.log_in_frame)
 
-        self.f1_entry1 = customtkinter.CTkEntry(master=self.frame1, placeholder_text="Username")
-        self.f1_entry1.pack(pady=12, padx=10)
+        self.log_in_label = customtkinter.CTkLabel(master=self.log_in_frame, text="Login  System", font=("Roboto", 24))
+        self.log_in_label.pack(pady=12, padx=10)
 
-        self.f1_entry2 = customtkinter.CTkEntry(master=self.frame1, placeholder_text="Password")
-        self.f1_entry2.pack(pady=12, padx=10)
+        self.username_entry = customtkinter.CTkEntry(master=self.log_in_frame, placeholder_text="Username")
+        self.username_entry.pack(pady=12, padx=10)
 
-        self.f1_button = customtkinter.CTkButton(master=self.frame1, text="Login", command = self.login)
-        self.f1_button.pack(pady=12, padx=10)
+        self.password_entry = customtkinter.CTkEntry(master=self.log_in_frame, placeholder_text="Password")
+        self.password_entry.pack(pady=12, padx=10)
 
-        #checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember Me")
-        #checkbox.pack(pady=12, padx=10)
+        self.f1_button = customtkinter.CTkButton(master=self.log_in_frame, text="Login", command = self.login)
+        self.f1_button.pack(pady=12, padx=10)       
 
-        raise_frame(self.frame1)
+        raise_frame(self.log_in_frame)
+        
         self.root.mainloop()
 
 def raise_frame(frame):
+    
     frame.tkraise()
