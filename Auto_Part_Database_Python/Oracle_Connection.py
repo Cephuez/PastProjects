2ea7cbc9f8043922f65ae5d6eecbf7d37e4d6943
Cephuez <saul2004966@gmail.com>
2025-02-12 00:14:45 -0700

    Display Generator Code

--- Auto_Part_Database_Python/Oracle_Connection.py
+++ Auto_Part_Database_Python/Oracle_Connection.py
@@ -1,44 +1,19 @@
 import oracledb
 import pandas as pd
-from Input_Command import command
 from Data_Generator import code_generator
 from GUI_Application import gui_application
 
 from sqlalchemy import create_engine
 
-
-query = "1"
-#generator = code_generator("Name")
-#generator.log_in()
-#generator.genrate_order_dates()
-#generator.generate_orders()
-#input_command = command("Class")
-#input_command.print()
-#input_command.log_in() #-- Assume user has already logged in for debugging purpose
-#input_command.read_command()
+'''
+    This allows the start up of the code.
+    This will launch the application
+'''
 
 gui = gui_application("Name")
 gui.print()
 gui.log_in()
 gui.beginning_screen()
 
-
-
-'''
-TODOs
-1. When prompted, ask the user for their username and password
-2. Once logged in, show the commands avaialable for the user.
-    - Start Picking
-    - Stage Items
-    - Process Orders
-    - Move Parts
-    - Process Receiving Items
-3. Special inputs
-    - Exit = Leave Window
-
-4. Deal with exceptions if some inputs are incorrect.
-
-
-'''
 
 
