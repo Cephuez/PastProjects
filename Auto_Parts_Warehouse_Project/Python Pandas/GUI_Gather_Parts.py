99b3ea57b67ec071f1c64ac095a731fffee1d2d1
Cephuez <saul2004966@gmail.com>
2025-02-11 22:40:00 -0700

    Gathering Parts for Order
    This part of the application now works better. the worker can now navigate between all three methods I've coded.

--- Auto_Parts_Warehouse_Project/Python Pandas/GUI_Gather_Parts.py
+++ Auto_Parts_Warehouse_Project/Python Pandas/GUI_Gather_Parts.py
@@ -30,10 +30,11 @@ class Gather_Parts_Window:
         shelf_list = pd.read_sql(query, self.engine)
 
         if(shelf_list.size == 0):
-            self.load_staging_view(self.shelf_view_frame)
+            self.load_staging_view()
         else:
             product_id = shelf_list.iat[0,0]
             qty = shelf_list.iat[0,1]
+            self.product_id = product_id
             
             self.shelf_view_product_display.configure(text = "Scan Product: " + str(product_id))
             self.shelf_view_quantity_display.configure(text = "QTY: " + str(qty))
@@ -45,7 +46,8 @@ class Gather_Parts_Window:
         input_qty = self.shelf_view_quantity_entry.get()
 
         if(str(input_product_id) == str(self.product_id)):
-            query = "SELECT SUM(QUANTITY) FROM ORDERS_READY WHERE product_id = "+str(input_product_id)+" AND ZONE = '"+self.curr_stage+"'"
+            query = "SELECT SUM(QUANTITY) FROM ORDERS_READY WHERE ORDER_ID = "+self.order_id+\
+                    " AND PRODUCT_ID = "+str(input_product_id)+" AND ZONE = '"+self.curr_stage+"'"
             print(query)
             qty_result = pd.read_sql(query, self.engine).iat[0,0]
             print(qty_result)
@@ -60,17 +62,15 @@ class Gather_Parts_Window:
                 self.update_product_view()
                 print(query)
 
-    def display_product_in_shelf_view(self, other_frame):
-        if(other_frame is None):
-            self.staging_view_frame.destroy()
-        else:
-            other_frame.destroy()
+    def display_product_in_shelf_view(self):
+        self.curr_frame.destroy()
 
-        self.shelf_view_frame = customtkinter.CTkFrame(master = self.root)
-        self.shelf_view_frame.pack(pady=30, padx=10)
+        shelf_view_frame = customtkinter.CTkFrame(master = self.root)
+        shelf_view_frame.pack(pady=30, padx=10)
+        self.curr_frame = shelf_view_frame
 
-        self.shelf_view_label = customtkinter.CTkLabel(master=self.shelf_view_frame, text="Pick Part From Shelf", font=("Roboto", 34))
-        self.shelf_view_label.pack(pady=2, padx=10)
+        shelf_view_label = customtkinter.CTkLabel(shelf_view_frame, text="Pick Part From Shelf", font=("Roboto", 34))
+        shelf_view_label.pack(pady=10, padx=10)
 
         query = "SELECT PRODUCT_ID, COUNT(QUANTITY) FROM ORDERS_READY " \
                 "WHERE ORDER_ID = "+self.order_id+" AND ZONE = '"+self.curr_stage+"' GROUP BY PRODUCT_ID ORDER BY PRODUCT_ID"
@@ -80,28 +80,35 @@ class Gather_Parts_Window:
         self.product_id = shelf_list.iat[0,0]
         self.quantity = shelf_list.iat[0,1]        
 
-        self.shelf_view_stage_display = customtkinter.CTkLabel(master=self.shelf_view_frame, text=self.curr_stage, font=("Roboto", 20))
-        self.shelf_view_stage_display.pack(pady=2, padx=10)    
+        customtkinter.CTkLabel(shelf_view_frame, text=self.box_tote, font=("Roboto", 28)).pack(pady=10, padx=10)
         
-        self.shelf_view_product_display = customtkinter.CTkLabel(master=self.shelf_view_frame, text="Scan Product: " + str(self.product_id), font=("Roboto", 20))
-        self.shelf_view_product_display.pack(pady=2, padx=10)
+        customtkinter.CTkLabel(shelf_view_frame, text=self.curr_stage, font=("Roboto", 28)).pack(pady=10, padx=10)
 
-        self.shelf_view_product_entry = customtkinter.CTkEntry(master=self.shelf_view_frame)
-        self.shelf_view_product_entry.pack(pady=2, padx=10)
+        product_frame = customtkinter.CTkFrame(shelf_view_frame)
+        product_frame.pack(side='left', anchor='w', pady=30, padx=30)
+        
+        self.shelf_view_product_display = customtkinter.CTkLabel(product_frame, text="Scan Product: " + str(self.product_id), font=("Roboto", 20))
+        self.shelf_view_product_display.pack(side='top',pady=5, padx=10)
+
+        self.shelf_view_product_entry = customtkinter.CTkEntry(product_frame)
+        self.shelf_view_product_entry.pack(pady=5, padx=10)
         self.shelf_view_product_entry.bind('<Return>', self.check_product)
+        
+        quantity_frame = customtkinter.CTkFrame(shelf_view_frame)
+        quantity_frame.pack(side='right', anchor='e', pady=30, padx=30)
+        
+        self.shelf_view_quantity_display = customtkinter.CTkLabel(quantity_frame, text="QTY: " + str(self.quantity), font=("Roboto", 20))
+        self.shelf_view_quantity_display.pack(pady=5, padx=10)
 
-        self.shelf_view_quantity_display = customtkinter.CTkLabel(master=self.shelf_view_frame, text="QTY: " + str(self.quantity), font=("Roboto", 20))
-        self.shelf_view_quantity_display.pack(pady=2, padx=10)
-
-        self.shelf_view_quantity_entry = customtkinter.CTkEntry(master=self.shelf_view_frame, justify='center')
+        self.shelf_view_quantity_entry = customtkinter.CTkEntry(quantity_frame, justify='center', width = 40)
         self.shelf_view_quantity_entry.insert(0, '1')
-        self.shelf_view_quantity_entry.pack(pady=12, padx=10)
+        self.shelf_view_quantity_entry.pack(pady=5, padx=10)
         self.shelf_view_quantity_entry.bind('<Return>', self.check_product)
         self.shelf_view_quantity_entry.configure(state='disabled')
 
         self.root.bind('<F5>', self.enable_qty_input)
 
-        raise_frame(self.shelf_view_frame)
+        raise_frame(self.curr_frame)
 
     def enable_qty_input(self, event):
         state_condition = self.shelf_view_quantity_entry.cget("state")
@@ -112,83 +119,102 @@ class Gather_Parts_Window:
             if(len(num) == 0):
                 self.shelf_view_quantity_entry.insert(0, '1')
             self.shelf_view_quantity_entry.configure(state='disabled')
-        #self.bin_pick_frame_qty_entry.configure(state='disabled')
             
     def check_stage_input(self,event):
         stage_input = self.staging_view_entry.get()
         if(stage_input == self.curr_stage):
-            self.display_product_in_shelf_view(None)
+            self.display_product_in_shelf_view()
 
-    def load_staging_view(self, other_frame):
-        if(other_frame is None):
-            self.scan_box_frame.destroy()
-        else:
-            other_frame.destroy()
+    def load_staging_view(self):
+        self.curr_frame.destroy()
 
-        self.staging_view_frame = customtkinter.CTkFrame(master = self.root)
-        self.staging_view_frame.pack(pady=30, padx=10)
+        staging_view_frame = customtkinter.CTkFrame(master = self.root)
+        staging_view_frame.pack(pady=30, padx=10)
+        self.curr_frame = staging_view_frame
 
         query = "SELECT DISTINCT(ZONE) FROM ORDERS_READY WHERE ORDER_ID = "+self.order_id+" ORDER BY ZONE"
         get_stage_list = pd.read_sql(query, self.engine)
 
         if(get_stage_list.size == 0):
-            self.load_gather_parts_view(self.staging_view_frame)
+            self.root.unbind('<F3>', self.bind)
+            self.load_gather_parts_view()
         else:
             self.curr_stage = pd.read_sql(query, self.engine).iat[0,0]
 
-            stage_display = "Scan Stage: " + self.curr_stage
-            self.staging_view_display_stage = customtkinter.CTkLabel(master=self.staging_view_frame, text=stage_display, font=("Roboto", 34))
-            self.staging_view_display_stage.pack(pady=30, padx=10)
+            customtkinter.CTkLabel(staging_view_frame, text="Order ID: " + str(self.order_id), font=("Roboto", 35)).pack(side='top', pady=10, padx=10)
 
-            self.staging_view_entry = customtkinter.CTkEntry(master=self.staging_view_frame)
-            self.staging_view_entry.pack(pady=12, padx=10)
+            scan_stage_frame = customtkinter.CTkFrame(staging_view_frame)
+            scan_stage_frame.pack(side='top', pady=5, padx=10)
+            
+            stage_display = "Scan Stage: " + self.curr_stage
+            customtkinter.CTkLabel(scan_stage_frame, text=stage_display, font=("Roboto", 20)).pack(side='top', anchor='w',pady=10, padx=2)
+
+            self.staging_view_entry = customtkinter.CTkEntry(staging_view_frame)
+            self.staging_view_entry.pack(side='top', pady=12, padx=10)
             self.staging_view_entry.bind('<Return>', self.check_stage_input)  
 
-            raise_frame(self.staging_view_frame)
+            short_cut_frame = customtkinter.CTkFrame(staging_view_frame)
+            short_cut_frame.pack(side='bottom', pady=20, padx=20)
+
+            customtkinter.CTkLabel(short_cut_frame, text="F3. Exit", font=("Roboto", 20)).pack(side='left', anchor = 'w', pady=1, padx=30)        
+            self.bind = self.root.bind('<F3>', self.exit_load_staging_view)
+
+            raise_frame(self.curr_frame)
+
+    def exit_load_staging_view(self, event):
+        self.root.unbind('<F3>', self.bind)
+        self.curr_frame.destroy()
+        self.load_input_box_view()
         
 
     def check_input_box(self, event):
         self.box_tote = self.scan_box_entry.get()
 
-        print(self.box_tote)
         query = "SELECT COUNT(*) FROM APPROVED_ZONE WHERE ZONE = '"+self.box_tote+"'"
         tote_exists = pd.read_sql(query, self.engine).iat[0,0]
 
         query = "SELECT COUNT(*) FROM ORDER_LIST WHERE ZONE = '"+self.box_tote+"' AND ORDER_ID != " + str(self.order_id)
         empty_tote = pd.read_sql(query, self.engine).iat[0,0]
 
-        print(tote_exists)
-        print(empty_tote)
         if(int(tote_exists) == 1 and int(empty_tote) == 0):
-            print("Change")
-            self.load_staging_view(None)
+            self.load_staging_view()
             
         
-    def load_input_box_view(self, other_frame):
-        if(other_frame is None):
-            self.gather_parts_frame.destroy()
-        else:
-            other_frame.destory()
+    def load_input_box_view(self):
+        self.curr_frame.destroy()
 
-        self.scan_box_frame = customtkinter.CTkFrame(master = self.root)
-        self.scan_box_frame.pack(pady=30, padx=10)
+        box_display_frame = customtkinter.CTkFrame(master = self.root)
+        box_display_frame.pack(pady=30, padx=10)
+        self.curr_frame = box_display_frame
 
-        self.scan_box_display = customtkinter.CTkLabel(master=self.scan_box_frame, text="Scan Box: ", font=("Roboto", 34))
-        self.scan_box_display.pack(pady=30, padx=10)
+        customtkinter.CTkLabel(box_display_frame, text="Order ID: " + str(self.order_id), font=("Roboto", 35)).pack(side='top', pady=10, padx=10)
 
-        self.scan_box_entry = customtkinter.CTkEntry(master=self.scan_box_frame)
-        self.scan_box_entry.pack(pady=12, padx=10)
-        self.scan_box_entry.bind('<Return>', self.check_input_box)          
-        
-        #SELECT COUNT(*) FROM APPROVED_ZONE WHERE ZONE = 'ORDER_TOTE_00011'; -- If 1, then approved box
-        #SELECT COUNT(*) FROM ORDER_LIST WHERE ZONE = 'ORDER_TOTE_00001' -- If 0, then box can be used
+        scan_box_frame = customtkinter.CTkFrame(box_display_frame)
+        scan_box_frame.pack(side='top', anchor = 'w', pady=10, padx=30)
 
-        raise_frame(self.scan_box_frame)
+        customtkinter.CTkLabel(scan_box_frame, text="Scan Box: ", font=("Roboto", 20)).pack(side='left', anchor='w', pady=10, padx=10)
+
+        self.scan_box_entry = customtkinter.CTkEntry(scan_box_frame)
+        self.scan_box_entry.pack(side='left', anchor='w', pady=12, padx=10)
+        self.scan_box_entry.bind('<Return>', self.check_input_box)
+
+        short_cut_frame = customtkinter.CTkFrame(box_display_frame)
+        short_cut_frame.pack(side='bottom', pady=20, padx=20)
+
+        customtkinter.CTkLabel(short_cut_frame, text="F3. Exit", font=("Roboto", 15)).pack(side='left', anchor = 'w', pady=1, padx=30)        
+        self.bind = self.root.bind('<F3>', self.exit_load_input_box_view)
+
+        raise_frame(self.curr_frame)
+
+    def exit_load_input_box_view(self, event):
+        self.root.unbind('<F3>', self.bind)
+        self.curr_frame.destroy()
+        self.load_gather_parts_view()
 
     def check_order_id(self, event):
         self.order_id = self.gather_parts_enter_product.get()
         if(int(self.order_id) in self.order_list):
-            self.load_input_box_view(None)
+            self.load_input_box_view()
             print("Correct Order ID")
         else:
             print("Wrong Order ID")
@@ -197,33 +223,79 @@ class Gather_Parts_Window:
         self.curr_frame.destroy()
 
         self.order_list = []
-        self.gather_parts_frame = customtkinter.CTkFrame(master = self.root)
-        self.gather_parts_frame.pack(pady=30, padx=10)
-        self.curr_frame = self.gather_parts_frame
+        self.i = 0
+        self.box_tote = ""
+        gather_parts_frame = customtkinter.CTkFrame(master = self.root)
+        gather_parts_frame.pack(pady=25, padx=10)
+        self.curr_frame = gather_parts_frame
 
-        self.gather_parts_label = customtkinter.CTkLabel(master=self.gather_parts_frame, text="Orders Ready List", font=("Roboto", 34))
-        self.gather_parts_label.pack(pady=30, padx=10)
+        customtkinter.CTkLabel(gather_parts_frame, text="Orders Ready List", font=("Roboto", 34)).pack(side='top', pady=30, padx=10)
 
-        self.gather_parts_enter_product = customtkinter.CTkEntry(master=self.gather_parts_frame)
-        self.gather_parts_enter_product.pack(pady=12, padx=10)
+        enter_order_id_frame = customtkinter.CTkFrame(gather_parts_frame)
+        enter_order_id_frame.pack(side='top', anchor = 'w', pady=5, padx=30)
+
+        customtkinter.CTkLabel(enter_order_id_frame, text="Order: ", font=("Roboto", 20)).pack(side='left', anchor = 'w', pady=1, padx=10)
+
+        self.gather_parts_enter_product = customtkinter.CTkEntry(enter_order_id_frame)
+        self.gather_parts_enter_product.pack(pady=1, padx=1)
         self.gather_parts_enter_product.bind('<Return>', self.check_order_id)   
 
         get_orders_query = "SELECT DISTINCT(ORDER_ID) FROM ORDERS_READY ORDER BY ORDER_ID"
         all_order_list = pd.read_sql(get_orders_query, self.engine)
         list_length = all_order_list.size
 
+        self.order_id_list_frame = customtkinter.CTkFrame(gather_parts_frame)
+        self.order_id_list_frame.pack(side='top', anchor = 'w', pady=10, padx=30)
+        
         for i in range(list_length):
-            if(i == 7):
-                break
             order_id = all_order_list.iat[i,0]
             self.order_list.append(int(order_id))
-            self.gather_parts_label = customtkinter.CTkLabel(master=self.gather_parts_frame, text="Order: " + str(order_id), font=("Roboto", 20))
-            self.gather_parts_label.pack(pady=2, padx=10)            
 
-        raise_frame(self.gather_parts_frame)
+        self.temp_label = []
+        self.display_order_list()
+
+        short_cut_frame = customtkinter.CTkFrame(gather_parts_frame)
+        short_cut_frame.pack(side='bottom', pady=2, padx=20)
+
+        customtkinter.CTkLabel(short_cut_frame, text="F3. Exit", font=("Roboto", 20)).pack(side='left', anchor = 'w', pady=1, padx=5)        
+        self.bind_exit = self.root.bind('<F3>', self.exit_window)
+
+        customtkinter.CTkLabel(short_cut_frame, text="F5. Prev", font=("Roboto", 20)).pack(side='left', anchor = 'w', pady=1, padx=5)        
+        self.bind_exit = self.root.bind('<F5>', self.prev_page)
+
+        customtkinter.CTkLabel(short_cut_frame, text="F7. Next", font=("Roboto", 20)).pack(side='left', anchor = 'w', pady=1, padx=5)        
+        self.bind_exit = self.root.bind('<F7>', self.next_page)
+        
+        raise_frame(self.curr_frame)
+
+    def display_order_list(self):
+        if(len(self.temp_label) > 0 ):
+            for label in self.temp_label:
+                label.destroy()
+            self.temp_label = []
+        min_i = self.i
+        for i in range(min_i, len(self.order_list)):
+            if(i == min_i+6):
+                break
+            order_id = self.order_list[i]
+            order_label = customtkinter.CTkLabel(self.order_id_list_frame, text="Order: " + str(order_id), font=("Roboto", 20))
+            order_label.pack(side='top', anchor = 'w', pady=2, padx=10)
+            self.temp_label.append(order_label)
+
+    def prev_page(self, event):
+        self.i = self.i - 6
+        if(self.i < 0):
+            self.i = 0
+        self.display_order_list()
+
+    def next_page(self, event):
+        if(self.i + 6 < len(self.order_list)):
+            self.i = self.i + 6
+        self.display_order_list()
+
         
     def exit_window(self, event):
-        self.root.unbind('<F3>', self.bind)
+        self.root.unbind('<F3>', self.bind_exit)
         self.curr_frame.destroy()
         self.gui_application.display_command_window()
         
